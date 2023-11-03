997a08775b39e717ed35ba0e3824f45faeb9d3bf
Olamide Alokan <alokanolamide@gmail.com>
2023-11-03 12:26:52 +0100

    added tire functionality

--- car.py
+++ car.py
@@ -4,13 +4,14 @@ Car class definition
 from servicable import Servicable
 
 class Car(Servicable):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tire):
         """
-        car constructor with engine and battery property
+        car constructor with engine, battery and tire property
         """
         self.engine = engine
         self.battery = battery
+        self.tire = tire
 
     @abstractmethod
     def needs_service(self):
-        return self.engine.needs_service() or self.battey.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service() or self.tire.needs_service()
